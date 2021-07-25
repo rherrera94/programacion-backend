f1cac98b7901a05e71a86ce58e953074a635e393
rherrera94 <rafael.herrera@outlook.com.ar>
2021-07-25 09:31:52 -0300

    desafio 12

--- desafio12/public/index.js
+++ desafio12/public/index.js
@@ -1,5 +1,7 @@
 
 const socket=io();
+/*el callback que le paso a vistaTabla lo que va a hacer es tomar el elemento de id
+tablita y le va a poner lo que me entrega vistaTabla*/
 socket.on('productos',productos =>{
     vistaTabla(productos, html => {
         document.getElementById('tablita').innerHTML = html;
@@ -23,10 +25,7 @@ form.addEventListener('submit', evento => {
     sacada del formulario.
     */
     fetch('/productos/guardar', {
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        method: 'POST',
+        headers: {'Content-Type': 'application/json'},method: 'POST',
         body: JSON.stringify(productoInfo)
     }) 
     .then(respuesta =>respuesta.json()) 
@@ -36,7 +35,9 @@ form.addEventListener('submit', evento => {
     })
     .catch(error => console.error(error.message));
 })
-
+/* en esta funcion tomo lo que me viene por productos, lo inserto dentro del
+handlebars de tablaProductos y despues paso esa info al callback que despues
+unira este pedazo de html al html principal*/
 function vistaTabla(productos,cb) {
     fetch('/tablaProductos.hbs')
     .then(respuesta => respuesta.text())
